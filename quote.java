5adb1f2b775bedc06c0d51a97e7a7c904be39281
AISWARYA R <aiswarya06211@gmail.com>
2026-08-04 21:21:42 +0530

    Update quote.java

--- quote.java
+++ quote.java
@@ -5,7 +5,7 @@ class QuoteGenerator {
         String[] quotes = {
             "Life is not about what we expect, it's about what we accept.!",
             "Dream big, work hard, stay humble.!",
-            "Code is like humor. When you have to explain it, it’s bad.",
+            "Code is like humor. When you have to explain it, it’s bad.!",
             "Success is not final, failure is not fatal: it is the courage to continue that counts.",
             "Do something today that your future self will thank you for."
         };
