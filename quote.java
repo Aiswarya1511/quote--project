9b7f676fda6fbc0c2c911bd09eb62447d1d0605d
AISWARYA R <aiswarya06211@gmail.com>
2026-08-04 21:22:00 +0530

    Update quote.java

--- quote.java
+++ quote.java
@@ -6,7 +6,7 @@ class QuoteGenerator {
             "Life is not about what we expect, it's about what we accept.!",
             "Dream big, work hard, stay humble.!",
             "Code is like humor. When you have to explain it, it’s bad.!",
-            "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+            "Success is not final, failure is not fatal: it is the courage to continue that counts.!",
             "Do something today that your future self will thank you for."
         };
 
