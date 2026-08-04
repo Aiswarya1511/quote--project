47377ba7d40f349e16bbb7bc6a4e3d0c659be90a
AISWARYA R <aiswarya06211@gmail.com>
2026-08-04 21:21:26 +0530

    Update quote.java

--- quote.java
+++ quote.java
@@ -4,7 +4,7 @@ class QuoteGenerator {
     public static void main(String[] args) {
         String[] quotes = {
             "Life is not about what we expect, it's about what we accept.!",
-            "Dream big, work hard, stay humble.",
+            "Dream big, work hard, stay humble.!",
             "Code is like humor. When you have to explain it, it’s bad.",
             "Success is not final, failure is not fatal: it is the courage to continue that counts.",
             "Do something today that your future self will thank you for."
